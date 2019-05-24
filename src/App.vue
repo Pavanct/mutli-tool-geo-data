058fe99ef3db26e46beefb499bc5a65bc1e19a6a
Hassan Chamseddine <chamseddine@campus.tu-berlin.de>
2019-05-24 13:33:12 +0200

    adding Maps component

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,15 @@
 <template>
-  <div style="height: 100%; overflow: auto; position: unset;">
-
-    <l-map id="l-map"
-      :zoom="zoom"
-      :center="center"
-      :options="mapOptions"
-      style="height: 100%"
-      @update:center="centerUpdate"
-      @update:zoom="zoomUpdate"
-    >
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <l-marker :lat-lng="withPopup">
-        <l-popup>
-          <div @click="innerClick">
-            I am a popup
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
-          </div>
-        </l-popup>
-      </l-marker>
-      <l-marker :lat-lng="withTooltip">
-        <l-tooltip :options="{ permanent: true, interactive: true }">
-          <div @click="innerClick">
-            I am a tooltip
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
-          </div>
-        </l-tooltip>
-      </l-marker>
-    </l-map>
-    </div>
+  <div id="app" style="height: 100%">
+    <Maps/>
+  </div>
 </template>
 
 <script>
-import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
-export default {
-  name: "Example",
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup,
-    LTooltip
-  },
-  data() {
-    return {
-      zoom: 13,
-      center: latLng(52.5138587,13.3187443),
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      withPopup: latLng(52.5138587,13.3187443),
-      withTooltip: latLng(52.5138587,13.3187443),
-      currentZoom: 11.5,
-      currentCenter: latLng(52.5138587,13.3187443),
-      showParagraph: false,
-      mapOptions: {
-        zoomSnap: 0.5
-      }
-    };
-  },
-  methods: {
-    zoomUpdate(zoom) {
-      this.currentZoom = zoom;
-    },
-    centerUpdate(center) {
-      this.currentCenter = center;
-    },
-    showLongText() {
-      this.showParagraph = !this.showParagraph;
-    },
-    innerClick() {
-      alert("Click!");
-    }
-  }
-};
-</script>
+import Maps from './components/Maps'
 
-<style>
-body, html{
-  height: 100%;
+export default {
+  components: {
+    Maps
+  }
 }
-#l-map{
-  height: 100%;
-}
-</style>
+</script>
--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,12 +1,86 @@
 <template>
-  <div>
-    
-  </div>
+  <div style="height: 100%; overflow: auto; position: unset;">
+
+    <l-map id="l-map"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 100%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker :lat-lng="withPopup">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+    </div>
 </template>
 
 <script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
 export default {
-  name: "Maps"
+  name: "Maps",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(52.5138587,13.3187443),
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(52.5138587,13.3187443),
+      withTooltip: latLng(52.5138587,13.3187443),
+      currentZoom: 11.5,
+      currentCenter: latLng(52.5138587,13.3187443),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      }
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  }
 };
 </script>
 
@@ -17,5 +91,11 @@ body,
 #maps {
   height: 100%;
   margin: 0;
+}
+body, html{
+  height: 100%;
+}
+#l-map{
+  height: 100%;
 }
 </style>
